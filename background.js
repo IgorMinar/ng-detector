b3dc150e43b595cd27026bd240096403b241195d
Paul Leclercq <paleclercq@gmail.com>
2014-11-11 23:19:29 +0100

    Page action click makes appear a popup containing current tab angular version and all others angular versions met while browsing

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 console.log('ng-detector background script started!');
 
+var versions = []; //will contains all version of angular apps met, array index is tab.id
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (typeof request === 'string') {
@@ -11,16 +13,38 @@ chrome.runtime.onMessage.addListener(
     var versionObject = request.angularVersion;
 
     if (versionObject) {
-      var version = versionObject.codeName
-                    ? 'AngularJS ' + versionObject.full + '\n' + versionObject.codeName
-                    : 'Angular'; // maybe AngularDart or lazy-loaded AngularJS?
+      var url = sender.tab.url,
+          tabId = sender.tab.id,
+          txt = versionObject.codeName
+                        ? 'AngularJS ' + versionObject.full + '\n' + versionObject.codeName
+                        : 'Angular'; // maybe AngularDart or lazy-loaded AngularJS?
 
-      chrome.pageAction.show(sender.tab.id);
-      chrome.pageAction.setTitle({
-        tabId: sender.tab.id,
-        title: 'Super-powered by \n' + version
+      versions[sender.tab.id] = {};
+
+      versions[sender.tab.id].url = url;
+      versions[sender.tab.id].tabId = tabId;
+      versions[sender.tab.id].txt = txt;
+
+      // duplicate case, if the user open the same angular app in a different tab we do not store the data again
+      var unique = versions.filter(function(v) {
+          return v.url === url;
       });
+      versions[sender.tab.id].duplicate = unique.length > 1;
 
-      console.log(version + 'found at:', sender.tab.url);
+      chrome.pageAction.setTitle({
+        tabId: tabId,
+        title: 'Super-powered by \n' + txt
+      });
+      chrome.pageAction.show(tabId);
     }
   });
+
+/**
+ * @description
+ * get all versions of angular met while browsing indexed by tab.id
+ * used in popup.js to display version in popup.html
+ * @returns {array}
+ */
+function getVersions(){
+  return versions;
+}
---
+++ js/popup.js
@@ -0,0 +1,38 @@
+//This JS file will load from background.js the angular version of the current tab and others angular versions met while browsing.
+
+//get current tab.id
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
+    var bkg = chrome.extension.getBackgroundPage(); //get to versions array from background page
+    var versions = bkg.getVersions(); // our custom method in background.js
+
+    //build table data for all versions met
+    var table = "" +
+    "<table class='table-bordered'>"+
+    "  <caption>Angular apps met<caption>" +
+    "  <th>"+
+    "    Version"+
+    "  </th>"+
+    "  <th>"+
+    "    URL"+
+    "  </th>";
+
+    versions.forEach(function(v){
+      if(!v.duplicate){ //avoid duplicate table entry
+        table += ""+
+        "  <tr>" +
+          "    <td>"+
+                v.txt +
+          "    </td>"+
+          "    <td>"+
+                v.url +
+          "    </td>"+
+        "  </tr>";
+      }
+    });
+
+    table += "</table>";
+
+    //display in popup.html
+    document.getElementById("version").innerHTML = versions[tabs[0].id].txt; //current angular app
+    document.getElementById("data").innerHTML = table; //others angular app met while browsing
+});
